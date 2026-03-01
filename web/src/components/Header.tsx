656ae37cb3f6f51165385e9da60a92b09f7633fc
sevda-m-16214 <118723080+sevda-m-16214@users.noreply.github.com>
2026-03-01 17:09:17 +0400

    Added Navigation and About section

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
       className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 
       ${
         scrolled
-          ? "bg-white shadow-md py-3"
+          ? "bg-accent-bordeaux shadow-md py-3"
           : "bg-transparent py-5"
       }`}
     >
@@ -28,7 +28,7 @@ const Header = () => {
         <Link
           to="/"
           className={`font-bold text-xl transition-colors duration-300 ${
-            scrolled ? "text-black" : "text-white"
+            scrolled ? "text-white" : "text-white"
           }`}
         >
           Heritage Maps
@@ -38,31 +38,26 @@ const Header = () => {
         <nav className="hidden md:flex items-center gap-8">
           <Link
             to="/"
-            className={`text-sm font-medium transition-colors ${
-              scrolled ? "text-gray-700 hover:text-black" : "text-white hover:text-gray-300"
-            }`}
+            className={`text-sm font-medium transition-colors 
+              ${"text-white hover:text-gray-300"}`}
           >
             Home
           </Link>
           <Link
             to="/advanced-search"
-            className={`text-sm font-medium transition-colors ${
-              scrolled ? "text-gray-700 hover:text-black" : "text-white hover:text-gray-300"
-            }`}
+            className={`text-sm font-medium transition-colors 
+              ${"text-white hover:text-gray-300"}`}
           >
             Advanced Search
           </Link>
           
             <Link
             to="/sign-in"
-            className={`ml-4 inline-flex items-center text-sm font-medium px-5 py-2 rounded-md transition ${
-                scrolled
-                    ? "border border-gray-400 text-black hover:bg-gray-100"
-                    : "border border-white text-white hover:bg-white/20"
-            }`}
-        >
-    Login / Sign In
-  </Link>
+            className={`ml-4 inline-flex items-center text-sm font-medium px-5 py-2 rounded-md transition
+               ${"border border-white text-white hover:bg-white/20"}`}
+            >
+              Login / Sign In
+            </Link>
             
         </nav>
       </div>
--- web/src/pages/LandingPage.tsx
+++ web/src/pages/LandingPage.tsx
@@ -1,22 +1,109 @@
 import heroImage from "../assets/images/Landing page hero.jpg";
+import architectImage from "../assets/images/architectsearch.png";
+import eraImage from "../assets/images/SEARCH-BY-ERA.jpg";
+
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
     return (
-        <div
-            className="relative min-h-screen bg-cover bg-center flex items-center justify-center"
-            style={{ backgroundImage: `url(${heroImage})`}}
-        >
-            <div className="absolute inset-0 bg-black/50"></div>
+        <>
+            <section
+                className="min-h-screen bg-cover bg-center flex items-center justify-center"
+                style={{ backgroundImage: `url(${heroImage})`}}
+            >
+                <div className="absolute inset-0 bg-black/50"></div>
 
+                {/* Centered Text */}
+                <div className="relative z-10 text-center text-white px-6">
+                    <h1 className="text-center text-4xl md:text-6xl font-bold mb-4">
+                    Welcome to Heritage Maps 
+                    </h1>
+                </div>
+            </section>
 
-            {/* Centered Text */}
-            <div className="relative z-10 text-center text-white px-6">
-                <h1 className="text-center text-4xl md:text-6xl font-bold mb-4">
-                   Welcome to Heritage Maps 
-                </h1>
+            <section className="py-24 bg-bg-seashell">
+                <div className="max-w-6xl mx-auto px-6">
 
-            </div>
-        </div>
+                    {/* Section Title */}
+                    <h2 className="text-4xl font-bold mb-12 text-accent-bordeaux">
+                        About the Project
+                    </h2>
+
+                    {/* Content Row */}
+                    <div className="flex items-center gap-12">
+                    
+                        {/* Left Column (Text) */}
+                        <div className="flex-1">
+                            <h3 className="text-2xl font-bold text-amber-900 mb-4">
+                            An Interactive Database of Baku's Architectural Landmarks
+                            </h3>
+                        </div>
+
+                        {/* Right Column (Image Placeholder) */}
+                        <div className="flex-1">
+                            <div className="h-64 bg-gray-300 rounded-lg"></div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            {/* Database Navigation */}
+            <section className="py-24 bg-bg-seashell">
+                <div className="max-w-6xl mx-auto px-6">
+                    <h2 className="text-4xl font-bold mb-7 text-accent-bordeaux">
+                        Browse the Database by
+                    </h2>
+
+                    <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+                        <Link
+                            to="/search-by-architects"
+                            className="group relative h-64 rounded-xl overflow-hidden"
+                        >
+                            {/* Background Image*/}
+                            <img
+                                src={architectImage}
+                                alt="Search by Architects"
+                                className="absolute inset-0 w-full h-full object-cover"
+                            />
+                            {/* Dark Overlay*/}
+                            <div className="absolute insert-0 bg-black/80 group-hover:bg-black/50 transition"/>
+
+                            {/* Text Label*/}
+                            <div className="relative z-10 flex items-center justify-center h-full">
+                                <h3 className="text-3xl font-bold text-accent-bordeaux">
+                                    Architect
+                                </h3>
+                            </div>
+
+                        </Link>
+
+                        <Link
+                            to="/search-by-era"
+                            className="group relative h-64 rounded-xl overflow-hidden"
+                        >
+                            {/* Background Image*/}
+                            <img
+                                src={eraImage}
+                                alt="Search by Era"
+                                className="absolute inset-0 w-full h-full object-cover"
+                            />
+                            {/* Dark Overlay*/}
+                            <div className="absolute insert-0 bg-black/10 group-hover:bg-black/50 transition"/>
+
+                            {/* Text Label*/}
+                            <div className="relative z-10 flex items-center justify-center h-full">
+                                <h3 className="text-3xl font-bold text-accent-bordeaux">
+                                    Era
+                                </h3>
+                            </div>
+
+                        </Link>
+                    </div>
+
+                </div>
+            </section>
+        </>
     );
 };
 
